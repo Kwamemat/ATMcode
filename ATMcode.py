2b098949255981c1fc6a522b9f60bac7ae5aad35
Kofi <91669601+kofidako@users.noreply.github.com>
2021-10-11 21:11:07 +0000

    Update ATMcode.py

--- ATMcode.py
+++ ATMcode.py
@@ -1,25 +1,43 @@
 #This is the dictionary that holds user information
 userDetails = {'kojo': 
                 {'pin':'1234', 
-                'balance' : {'GHS' : 6000000,
-                             'USD' : 1000000}},
+                'balance' : {'GHS' : 35190,
+                             'USD' : 5865}},
                 'kofi': 
                 {'pin':'1655', 
-                'balance' : {'GHS' : 1350000,
-                             'USD' : 9056000}},
+                'balance' : {'GHS' : 39000,
+                             'USD' : 6500}},
                 'kwabena': 
                 {'pin':'0480', 
-                'balance' : {'GHS' : 9876700,
-                             'USD' : 1986600}},
+                'balance' : {'GHS' : 20814,
+                             'USD' : 3469}},
                 'matrevi': 
                 {'pin':'1306', 
-                'balance' : {'GHS' : 8797800,
-                             'USD' : 5630000}},
+                'balance' : {'GHS' : 26856,
+                             'USD' : 4476}},
                'justin':
                {'pin':'8743', 
-                'balance' : {'GHS' : 3423000,
-                             'USD' : 1096600}}
+                'balance' : {'GHS' : 35196,
+                             'USD' : 5866}},
+               'fernanda':
+               {'pin':'5047'},
+                'balance' : {'GHS' : 790665,
+                             'USD' : 7989}},
+                'gifty':
+                'balance' : {'GHS' : 34212,
+                             'USD' : 5702}},
+                'exorm':
+                'balance': {'GHS' : 323154,
+                            'USD' : 3859}},
+                'sampson':
+                'balance': {'GHS' : 26322,
+                            'USD' : 4387}},
+                'georgina':
+                'balance': {'GHS' : 31974,
+                            'USD' : 5329}},
                }
+
+            
 
 #This variable is set to false by default but changed to true when a user is validated for a session
 is_logged_in = False
@@ -29,4 +47,11 @@ username = None
 currency = None
 
 
+#This function is called to verify user credentials and returns a boolean value 
+ #depending on the validity of credentials entered
+ def user_is_valid(username, pin):
+     if username in userDetails:
+         if pin == userDetails[username]['pin']:
+             return True
 
+     return False
